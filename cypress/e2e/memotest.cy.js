3786b06d5844d1e312563131317180c35ac5e2d4
Javier Anibal Villca <javieranibalvillca@gmail.com>
2023-01-07 22:23:40 -0300

    actualizado cypress

--- cypress/e2e/memotest.cy.js
+++ cypress/e2e/memotest.cy.js
@@ -7,28 +7,30 @@ context("Memotest",()=>{
         cy.visit(URL);
     })
 
-    it("revisamos que haya 12 cuadros en la tabla",()=>{
-        cy.get(".cuerpo").find(".cuadro").should("have.length", NUMERO_CUADROS);
-    });
+    describe('juega al memotest', () => { 
+        it("revisamos que haya 12 cuadros en la tabla",()=>{
+            cy.get(".cuerpo").find(".cuadro").should("have.length", NUMERO_CUADROS);
+        });
 
-    it("aseguramos que los cuadros sean aleatorios",()=>{
-        cy.get(".cuadro").then((cuadros)=>{
-            let clasesOriginales = [];
-            cuadros.each((index, cuadro)=>{
-                clasesOriginales.push(cuadro.className);
-            });
-            
-            cy.visit(URL);
-
-            let clasesNuevas = [];
-            cy.get(".cuadro").then((nuevosCuadros)=>{
-                nuevosCuadros.each((index, cuadro)=>{
-                    clasesNuevas.push(cuadro.className);
+        it("aseguramos que los cuadros sean aleatorios",()=>{
+            cy.get(".cuadro").then((cuadros)=>{
+                let clasesOriginales = [];
+                cuadros.each((index, cuadro)=>{
+                    clasesOriginales.push(cuadro.className);
                 });
-                cy.wrap(clasesOriginales).should("to.deep.equal", clasesNuevas); //ahora usamos to.deep.equal por que ya no existe not.deep.equal
+                
+                cy.visit(URL);
+
+                let clasesNuevas = [];
+                cy.get(".cuadro").then((nuevosCuadros)=>{
+                    nuevosCuadros.each((index, cuadro)=>{
+                        clasesNuevas.push(cuadro.className);
+                    });
+                    cy.wrap(clasesOriginales).should("to.deep.equal", clasesNuevas); //ahora usamos to.deep.equal por que ya no existe not.deep.equal
+                });
             });
         });
+        //seguir en 1:30, 
     });
-    //seguir en 1:30, 
 
 });
